6faf0f8978c2fc6d9a0d46727e2070862a6226e6
Andy Halterman <ahalterman0@gmail.com>
2017-01-30 17:04:52 -0500

    Docker and volumes and echos

--- create_index.sh
+++ create_index.sh
@@ -1,2 +1,16 @@
-# make the index
+echo "Downloading Geonames gazetteer..."
+#wget http://download.geonames.org/export/dump/allCountries.zip
+echo "Unpacking Geonames gazetteer..."
+#unzip allCountries.zip
+
+echo "Starting Docker container and data volume..."
+sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/data/:/usr/share/elasticsearch/data elasticsearch
+sleep 10s
+
+echo "Creating mappings for the fields in the Geonames index..."
 curl -XPUT 'localhost:9200/geonames' -H 'Content-Type: application/json' -d @geonames_mapping.json
+
+echo "Loading gazetteer into Elasticsearch..."
+python geonames_elasticsearch_loader.py
+
+echo "Done"
