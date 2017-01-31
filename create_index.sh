ed33dea555136334927e20b8456bf4f15c85f8ca
Andy Halterman <ahalterman0@gmail.com>
2017-01-31 14:45:09 -0500

    Fixed file for running and improved docs

--- create_index.sh
+++ create_index.sh
@@ -4,7 +4,7 @@ echo "Unpacking Geonames gazetteer..."
 #unzip allCountries.zip
 
 echo "Starting Docker container and data volume..."
-sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/data/:/usr/share/elasticsearch/data elasticsearch
+sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data elasticsearch:5.1.2
 sleep 10s
 
 echo "Creating mappings for the fields in the Geonames index..."
