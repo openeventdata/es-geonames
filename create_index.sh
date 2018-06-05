6df1453e3f71be3738e15e434e39a83dea9aad89
Andy Halterman <ahalt@kingston.cs.ou.edu>
2018-06-05 17:09:19 -0500

    Reorder steps to give container time to start. Closes #4

--- create_index.sh
+++ create_index.sh
@@ -1,12 +1,10 @@
+echo "Starting Docker container and data volume..."
+sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data elasticsearch:5.5.2
+
 echo "Downloading Geonames gazetteer..."
 wget http://download.geonames.org/export/dump/allCountries.zip
 echo "Unpacking Geonames gazetteer..."
 unzip allCountries.zip
-
-echo "Starting Docker container and data volume..."
-sudo docker run -d -p 127.0.0.1:9200:9200 -v $PWD/geonames_index/:/usr/share/elasticsearch/data elasticsearch:5.5.2
-
-sleep 10
 
 echo "Creating mappings for the fields in the Geonames index..."
 curl -XPUT 'localhost:9200/geonames' -H 'Content-Type: application/json' -d @geonames_mapping.json
